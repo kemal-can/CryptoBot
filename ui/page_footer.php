f50d6155b0afd229f56913b85c8f672d980110cf
Kemal CAN <mail@kemalcan.net>
2022-11-13 09:37:34 +0100

    Cryptocurrency Trading init

---
+++ ui/page_footer.php
@@ -0,0 +1,3 @@
+<div class="text-center cryptobot-margin-bottom-20">
+    <p class="cryptobot-margin-top-10">&copy; <?php echo date('Y', time()) > 2021 ? '2022 - ' : ''; ?>2022 CryptoBot For Binance</p>
+</div>
